ff08bb11791a2fa3949704765d4d29f7ed417440
HarbyT <harby.trujillo@gmail.com>
2024-10-31 19:39:12 -0500

    se agrega pantalla propuesta para login y para signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,43 @@
-import styles from "../page.module.css";
+'use client';
 
-export default function Signup() {
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    // Aquí iría la lógica para enviar los datos al backend
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Hola signup</h1>
-      </main>
+    <div className={styles.container}>   
+      <div className={styles.logo}>
+        <img src="/hacky.svg" alt="Logo de Hacky" width={100} height={100}/> 
+      </div>
+      <h2 className={styles.title}>Crea una contraseña</h2>
+      <p className={styles.subtitle}>Debe tener al menos 10 caracteres.</p><br /> {/* Agrega la descripción */}
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="password" className={styles.label}>Contraseña</label> <br /> <br />
+          <input type="password" id="password" className={styles.input} value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="confirmPassword" className={styles.label}>Confirmar contraseña</label> <br /> <br />
+          <input type="password" id="confirmPassword" className={styles.input} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+        </div>
+        <button type="submit" className={styles.button}>Continuar →</button>
+        <br />
+        <p className={styles.terms}>Al crear una cuenta en Hacky, aceptas los Términos de Servicio y Políticas de privacidad.</p> {/* Agrega los términos */}
+      </form>
     </div>
   );
 }
+
+export default LoginForm;
