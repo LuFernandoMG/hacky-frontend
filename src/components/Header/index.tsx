603ecec3a480ec157d607e3a5b8a901f29d3aa41
Luis Fernando Méndez <luisfernando.mendez5@gmail.com>
2024-11-04 21:35:08 -0300

    fix: general style adjustments

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 // components/Header.tsx
 "use client";
+import { usePathname } from "next/navigation";
 import styles from "./styles.module.css";
-//import Nav from '../Nav';
 import Image from 'next/image';
 
 const Header: React.FC = () => {
+    const pathname = usePathname();
+
+    if (pathname === "/login" || pathname === "/signup") {
+        return null;
+    }
     return (
         <header className={styles.header}>
             <div className={styles["header-container"]}>
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,38 +6,68 @@ import { HiBeaker, HiHome } from "react-icons/hi";
 import styles from "./styles.module.css";
 
 const Nav: React.FC = () => {
-    const pathname = usePathname();
-    console.log('pathname', pathname);
+  const pathname = usePathname();
 
+  if (pathname === "/login" || pathname === "/signup") {
+    return null;
+  }
   return (
     <nav className={styles["side-nav"]}>
       <ul className={styles["nav-list"]}>
         <Link href="/">
-          <li className={pathname === '/' ? styles["nav-item-active"] : styles["nav-item"]}>
+          <li
+            className={
+              pathname === "/" ? styles["nav-item-active"] : styles["nav-item"]
+            }
+          >
             <HiHome className={styles["nav-icon"]} />
             <span>Inicio</span>
           </li>
         </Link>
         <Link href="/invites">
-          <li className={pathname === '/invites' ? styles["nav-item-active"] : styles["nav-item"]}>
+          <li
+            className={
+              pathname === "/invites"
+                ? styles["nav-item-active"]
+                : styles["nav-item"]
+            }
+          >
             <MdInbox className={styles["nav-icon"]} />
             <span>Invitaciones</span>
           </li>
         </Link>
         <Link href="/project">
-          <li className={pathname === '/project' ? styles["nav-item-active"] : styles["nav-item"]}>
+          <li
+            className={
+              pathname === "/project"
+                ? styles["nav-item-active"]
+                : styles["nav-item"]
+            }
+          >
             <MdAutoStories className={styles["nav-icon"]} />
             <span>Mi Proyecto</span>
           </li>
         </Link>
         <Link href="/history">
-          <li className={pathname === '/history' ? styles["nav-item-active"] : styles["nav-item"]}>
+          <li
+            className={
+              pathname === "/history"
+                ? styles["nav-item-active"]
+                : styles["nav-item"]
+            }
+          >
             <MdAutoAwesomeMotion className={styles["nav-icon"]} />
             <span>Historial</span>
           </li>
         </Link>
         <Link href="/about">
-          <li className={pathname === '/about' ? styles["nav-item-active"] : styles["nav-item"]}>
+          <li
+            className={
+              pathname === "/about"
+                ? styles["nav-item-active"]
+                : styles["nav-item"]
+            }
+          >
             <HiBeaker className={styles["nav-icon"]} />
             <span>¿Qué es HACKY?</span>
           </li>
